0bf7a3869f663b590595915e5955679887f94aee
Gabriel Stackhouse <gabes@iastate.edu>
2019-02-08 20:46:04 -0600

    Finally got around to putting this on my personal repo.

---
+++ git.switch.sh
@@ -0,0 +1,10 @@
+git checkout master && git push origin master
+git checkout demo4 && git push origin demo4
+git checkout demo3 && git push origin demo3
+git checkout demo2 && git push origin demo2
+git checkout kevin-serverstuff && git push origin kevin-serverstuff
+git checkout ryanchat2 && git push origin ryanchat2
+git checkout adminpage && git push origin adminpage
+git checkout laser_visual && git push origin laser_visual
+git checkout lobby && git push origin lobby
+git checkout chatimprovements && git push origin chatimprovements
